09027dae977a7b141398f78015221b0541b3f7d9
Aycan <aycan.biilge@gmail.com>
2024-05-12 16:50:42 -0400

    added vehicle info reader to bufReader memthod

--- src/main/java/com/ps/DealerShipFileManager.java
+++ src/main/java/com/ps/DealerShipFileManager.java
@@ -12,12 +12,30 @@ public class DealerShipFileManager {
                 BufferedReader bufferedReader = new BufferedReader( new FileReader("inventory.csv"));
                 String line;
                 while((line = bufferedReader.readLine()) != null){
+                    String[] vehicle_info_storage = line.split("\\|");
+                    int vin = Integer.parseInt(vehicle_info_storage[0]);
+                    int year = Integer.parseInt(vehicle_info_storage[1]);
+                    String make = vehicle_info_storage[2];
+                    String model = vehicle_info_storage[3];
+                    String vehicle_type = vehicle_info_storage[4];
+                    String color = vehicle_info_storage[5];
+                    int odometer = Integer.parseInt(vehicle_info_storage[6]);
+                    double price = Double.parseDouble(vehicle_info_storage[7]);
+                    Vehicle temp_date = new Vehicle(vin, year, make, model, vehicle_type, color, odometer, price);
 
                 }
 
             }catch(IOException e){
                 e.printStackTrace();
             }
+
+        }
+
+        public static DealerShip getDealerShip(){
+            return null;
+        }
+
+        public static void saveDealerShip(DealerShip dealerShip){
 
         }
 
