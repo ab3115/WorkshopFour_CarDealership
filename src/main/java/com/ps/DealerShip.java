9b690d7b84bd207c2e5f1f51896aa92bb07d729b
Aycan <aycan.biilge@gmail.com>
2024-05-12 03:02:27 -0400

    Added methods to DealerShip class

--- src/main/java/com/ps/DealerShip.java
+++ src/main/java/com/ps/DealerShip.java
@@ -1,4 +1,37 @@
 package com.ps;
 
+import java.util.ArrayList;
+
 public class DealerShip {
+
+    private String name;
+    private String address;
+    private String phone;
+
+    static ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public void getVehiclesByPrice(int min, int max){
+
+    }
+
+    public void getVehicleByMakeModel(String make, String model){
+
+    }
+
+    public void getVehicleByYear(int min, int max){
+
+    }
+
+    public void getVehicleByColor(String color){
+
+    }
+
+    public void getVehicleByMileage(int min, int max){
+
+    }
+
+    public void getVehicleByType(String vehicleType){
+
+    }
+
 }
--- src/main/java/com/ps/DealerShipFileManager.java
+++ src/main/java/com/ps/DealerShipFileManager.java
@@ -1,8 +1,24 @@
 package com.ps;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class DealerShipFileManager {
 
+        public static void bufReader(){
 
+            try{
+                BufferedReader bufferedReader = new BufferedReader( new FileReader("inventory.csv"));
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+
+                }
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }
 
 }
