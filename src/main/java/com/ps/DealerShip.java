6a2cd34131d815ca94813fde0113f5dd18bbb33a
Aycan <aycan.biilge@gmail.com>
2024-05-12 16:35:22 -0400

    added return stubs and addVehicle method

--- src/main/java/com/ps/DealerShip.java
+++ src/main/java/com/ps/DealerShip.java
@@ -9,38 +9,42 @@ public class DealerShip {
     private String address;
     private String phone;
 
-    static ArrayList<Vehicle> inventory = new ArrayList<>();
+    static ArrayList<Vehicle> inventory;
+
+    public DealerShip(){
+        inventory = new ArrayList<>();
+    }
 
     public List<Vehicle> getVehiclesByPrice(int min, int max){
-
+        return null;
     }
 
     public List<Vehicle> getVehicleByMakeModel(String make, String model){
-
+        return null;
     }
 
     public List<Vehicle> getVehicleByYear(int min, int max){
-
+        return null;
     }
 
     public List<Vehicle> getVehicleByColor(String color){
-
+        return null;
     }
 
     public List<Vehicle> getVehicleByMileage(int min, int max){
-
+        return null;
     }
 
     public List<Vehicle> getVehicleByType(String vehicleType){
-
+        return null;
     }
 
     public List<Vehicle> getAllVehicles(){
-
+        return inventory;
     }
 
-    public void addVehicle(String vehicle){
-
+    public void addVehicle(Vehicle vehicle){
+        inventory.add(vehicle);
     }
 
     public void removeVehicle(String vehicle){
